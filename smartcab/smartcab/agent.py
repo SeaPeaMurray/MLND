654ef49672fdac8fcf7f3487cccf5aaac6b918a4
Clayton Murray <claypaulmurray@gmail.com>
2017-11-17 13:01:44 -0800

    cmon

--- smartcab/smartcab/agent.py
+++ smartcab/smartcab/agent.py
@@ -62,7 +62,7 @@ class LearningAgent(Agent):
         # With the hand-engineered features, this learning process gets entirely negated.
         
         # Set 'state' as a tuple of relevant data for the agent        
-        state = None
+        state = (inputs)
 
         return state
 
@@ -174,7 +174,7 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env, update_delay=0.01, display=False, log_metrics=True)
+    sim = Simulator(env, update_delay=0.01, display=True, log_metrics=True)
     
     ##############
     # Run the simulator
